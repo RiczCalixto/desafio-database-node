5a804f2365dcda65d9e2744b722b4ed8a27f839c
RiczCalixto <rcalixtos@gmail.com>
2020-07-16 00:11:20 -0300

    add transaction repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,24 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const { income, outcome } = transactions.reduce(
+      (accumulator: Omit<Balance, 'total'>, transaction: Transaction) => {
+        const transactionType = {
+          income: () => (accumulator.income += transaction.value),
+          outcome: () => (accumulator.outcome += transaction.value),
+        };
+        transactionType[transaction.type]();
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
