a288e259e7d2e57fd08b332bbbf0de6d2e51bad9
RiczCalixto <rcalixtos@gmail.com>
2020-07-16 00:11:44 -0300

    create new transaction with category

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import CreateTransactionService from '../services/CreateTransactionService';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -12,7 +13,19 @@ transactionsRouter.get('/', async (request, response) => {
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+  const createCategoryService = new CreateCategoryService();
+  const createTransactionService = new CreateTransactionService(
+    createCategoryService,
+  );
+  const transaction = await createTransactionService.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
